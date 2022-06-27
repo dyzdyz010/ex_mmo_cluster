149e48dfcfd86306ff29ecb8f3a48a5d9725fcc1
Du Yizhuo <dyzdyz010@gmail.com>
2022-06-27 10:27:56 +0800

    Add Tailwind support

--- apps/auth_server/config/dev.exs
+++ apps/auth_server/config/dev.exs
@@ -15,6 +15,7 @@ config :auth_server, AuthServerWeb.Endpoint,
   debug_errors: true,
   secret_key_base: "Hwf6mMFXHANzdiGEbEbDF3kESMesP2DgmYjmKihQM7uzDtXwrb987z2rKXT2Qvfo",
   watchers: [
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]},
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
     esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
   ]
